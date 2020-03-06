1ba7792f744fc51c36e3dd2455f2de62818af8d4
Pyroh <pyroh@icloud.com>
2020-03-06 17:12:42 +0100

    [ADD] transform an reassign operator
    [IMP] Documentation

--- Sources/SmoothOperators/Optionable.swift
+++ Sources/SmoothOperators/Optionable.swift
@@ -6,14 +6,49 @@
 //
 
 public extension Equatable {
+    
+    /// Returns `lhs` if it is equal to the result of `rhs`. `nil` otherwise.
+    /// - Parameters:
+    ///   - lhs: An `Equatable` value.
+    ///   - rhs: An expression of `Equatable` type.
+    /// - Returns: An optional carrying `lhs` or `nil`.
     @inlinable static func ==?(lhs: Self, rhs: @autoclosure () -> Self) -> Self? { lhs == rhs() ? lhs : nil }
+    
+    /// Returns `lhs` if it is not equal to the result of `rhs`. `nil` otherwise.
+    /// - Parameters:
+    ///   - lhs: An `Equatable` value.
+    ///   - rhs: An expression of `Equatable` type.
+    /// - Returns: An optional carrying `lhs` or `nil`.
     @inlinable static func !=?(lhs: Self, rhs: @autoclosure () -> Self) -> Self? { lhs != rhs() ? lhs : nil }
 }
 
 public extension Comparable {
+    
+    /// Returns `lhs` if it is greater than the result of `rhs`. `nil` otherwise.
+    /// - Parameters:
+    ///   - lhs: An `Comparable` value.
+    ///   - rhs: An expression of `Comparable` type.
+    /// - Returns: An optional carrying `lhs` or `nil`.
     @inlinable static func >?(lhs: Self, rhs: @autoclosure () -> Self) -> Self? { lhs > rhs() ? lhs : nil }
+    
+    /// Returns `lhs` if it is lesser than the result of `rhs`. `nil` otherwise.
+    /// - Parameters:
+    ///   - lhs: An `Comparable` value.
+    ///   - rhs: An expression of `Comparable` type.
+    /// - Returns: An optional carrying `lhs` or `nil`.
     @inlinable static func <?(lhs: Self, rhs: @autoclosure () -> Self) -> Self? { lhs < rhs() ? lhs : nil }
-        
+    
+    /// Returns `lhs` if it is greater than or equal to the result of `rhs`. `nil` otherwise.
+    /// - Parameters:
+    ///   - lhs: An `Comparable` value.
+    ///   - rhs: An expression of `Comparable` type.
+    /// - Returns: An optional carrying `lhs` or `nil`.
     @inlinable static func >=?(lhs: Self, rhs: @autoclosure () -> Self) -> Self? { lhs >= rhs() ? lhs : nil }
+    
+    /// Returns `lhs` if it is lesser than or equal to the result of `rhs`. `nil` otherwise.
+    /// - Parameters:
+    ///   - lhs: An `Comparable` value.
+    ///   - rhs: An expression of `Comparable` type.
+    /// - Returns: An optional carrying `lhs` or `nil`.
     @inlinable static func <=?(lhs: Self, rhs: @autoclosure () -> Self) -> Self? { lhs <= rhs() ? lhs : nil }
 }
--- Sources/SmoothOperators/SmoothOperators.swift
+++ Sources/SmoothOperators/SmoothOperators.swift
@@ -1,6 +1,7 @@
 precedencegroup ExponentationPrecedence {
     associativity: left
     higherThan: MultiplicationPrecedence
+    lowerThan: BitwiseShiftPrecedence
 }
 
 prefix operator ++
@@ -13,6 +14,8 @@ postfix operator --
 postfix operator -
 postfix operator +
 
+infix operator <-: AssignmentPrecedence
+
 infix operator **: ExponentationPrecedence
 
 infix operator >?: ComparisonPrecedence
@@ -23,3 +26,13 @@ infix operator <=?: ComparisonPrecedence
 
 infix operator ==?: ComparisonPrecedence
 infix operator !=?: ComparisonPrecedence
+
+
+/// Transform `lhs` using `rhs` and stores it in `lhs`.
+/// - Parameters:
+///   - lhs: A read write variable.
+///   - rhs: A transforming closure.
+@inlinable
+public func <-<T>(lhs: inout T, rhs: (T) throws -> T) rethrows {
+    lhs = try rhs(lhs)
+}
