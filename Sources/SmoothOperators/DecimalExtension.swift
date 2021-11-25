86e5bb8e732542346a7c71fa4a0b15e17f8b4324
Pyroh <pyroh@icloud.com>
2021-11-25 12:42:26 +0100

    [FIX] Documentation

--- Sources/SmoothOperators/DecimalExtension.swift
+++ Sources/SmoothOperators/DecimalExtension.swift
@@ -31,9 +31,9 @@ import Foundation
 
 public extension Decimal {
 
-    /// Performs a nan-coalescing operation, returning the value of `FloatingPoint` instance or a default value.
+    /// Performs a nan-coalescing operation, returning the value of `Decimal` instance or a default value.
     /// - Parameters:
-    ///   - floatingPointValue: A floating point value.
+    ///   - decimalValue: A decimal value.
     ///   - defaultValue: A value to use as a default. defaultValue and floatingPointValue have the same type.
     static func ??(decimalValue: Self, defaultValue: @autoclosure () throws -> Self) rethrows -> Self {
         decimalValue.isNaN ? try defaultValue() : decimalValue
