b770ea8174ef03f4872e8b21cbabd967079c8182
Pyroh <pyroh@icloud.com>
2022-05-20 16:00:29 +0200

    No comment...

--- Sources/SmoothOperators/SIMDExtension.swift
+++ Sources/SmoothOperators/SIMDExtension.swift
@@ -8,48 +8,52 @@
 import simd
 
 @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-extension SIMD2 where Scalar == Float {
-    @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
-    @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
-}
-@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-extension SIMD3 where Scalar == Float {
-    @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
-    @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
-}
-@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-extension SIMD4 where Scalar == Float {
-    @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
-    @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
-}
-@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-extension SIMD8 where Scalar == Float {
-    @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
-    @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
-}
-@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-extension SIMD16 where Scalar == Float {
+public extension SIMD2 where Scalar == Float {
     @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
     @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
 }
 
 @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-extension SIMD2 where Scalar == Double {
+public extension SIMD3 where Scalar == Float {
+    @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
+    @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
+}
+
+@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
+public extension SIMD4 where Scalar == Float {
+    @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
+    @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
+}
+
+@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
+public extension SIMD8 where Scalar == Float {
+    @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
+    @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
+}
+
+@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
+public extension SIMD16 where Scalar == Float {
+    @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
+    @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
+}
+
+@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
+public extension SIMD2 where Scalar == Double {
     @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
     @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
 }
 @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-extension SIMD3 where Scalar == Double {
+public extension SIMD3 where Scalar == Double {
     @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
     @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
 }
 @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-extension SIMD4 where Scalar == Double {
+public extension SIMD4 where Scalar == Double {
     @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
     @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
 }
 @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-extension SIMD8 where Scalar == Double {
+public extension SIMD8 where Scalar == Double {
     @inlinable static func **(lhs: Self, rhs: Self) -> Self { pow(lhs, rhs) }
     @inlinable static func **(lhs: Self, rhs: Scalar) -> Self { pow(lhs, .init(repeating: rhs)) }
 }
